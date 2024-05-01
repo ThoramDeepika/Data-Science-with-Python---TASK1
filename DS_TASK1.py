5af9fb68769ddb5c82116afd93f21981b9e653ba
ThoramDeepika <135530849+ThoramDeepika@users.noreply.github.com>
2024-05-01 16:50:28 +0530

    Add files via upload
    I have successfully completed my task1 at Main Flow Services and Technologies #mainflowservicesandtechnologies @mainflowservicesandtechnologies

---
+++ DS_TASK1.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# LIST
+
+
+# In[1]:
+
+
+# Creating a list
+my_list = [1, 2, 3, 4, 5]
+print("List:", my_list)
+
+
+# In[2]:
+
+
+# Adding elements to the list
+my_list.append(6)
+print("List after adding element:", my_list)
+
+
+# In[3]:
+
+
+# Removing elements from the list
+my_list.remove(3)
+print("List after removing element:", my_list)
+
+
+# In[10]:
+
+
+# Modifying elements in the list
+my_list[0] = 10
+print("List after modifying element:", my_list)
+
+
+# # DICTiONARY
+
+# In[5]:
+
+
+# Creating a dictionary
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+print("Dictionary:", my_dict)
+
+
+# In[6]:
+
+
+# Adding elements to the dictionary
+my_dict['d'] = 4
+print("Dictionary after adding element:", my_dict)
+
+
+# In[7]:
+
+
+# Removing elements from the dictionary
+del my_dict['b']
+print("Dictionary after removing element:", my_dict)
+
+
+# In[9]:
+
+
+# Modifying elements in the dictionary
+my_dict['a'] = 100
+print("Dictionary after modifying element:", my_dict)
+
+
+# # SET
+
+# In[11]:
+
+
+# Creating a set
+my_set = {1, 2, 3, 4, 5}
+print("Set:", my_set)
+
+
+# In[12]:
+
+
+# Adding elements to the set
+my_set.add(6)
+print("Set after adding element:", my_set)
+
+
+# In[13]:
+
+
+# Removing elements from the set
+my_set.remove(3)
+print("Set after removing element:", my_set)
+
+
+# In[ ]:
+
+
+
+
